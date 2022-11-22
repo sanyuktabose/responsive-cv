e94304b60285762d3c74621db00514332a308a5a
pritamprasd <mail@pritam.dev>
2022-11-22 15:01:08 +0530

    updating github gist

--- src/Page.jsx
+++ src/Page.jsx
@@ -8,7 +8,7 @@ import Education from './components/Education';
 import TechSkills from './components/TechSkills';
 
 const jsyaml = require('js-yaml');
-const config_url = 'https://gist.githubusercontent.com/pritamprasd/dc5f7deeccc69a21ef288e6903d833c7/raw/bb81ede17bcc3f2ff61ea5f67ba999af3265f4e1/cv.yaml'
+const config_url = 'https://gist.githubusercontent.com/pritamprasd/dc5f7deeccc69a21ef288e6903d833c7/raw/028bac4d35368eb74c9bc46640f2836eb95e1d5b/cv.yaml'
 
 export default function Page() {
   const [data, setdata] = useState({
